28e72437b41076ed059e36750121e8dd4f2b6cf1
kolergy <kolergy@gmail.com>
2025-01-05 12:39:30 +0100

    refactor: Add pylint disable comment for DebugPrint class to suppress R0903 warning

--- utils.py
+++ utils.py
@@ -2,7 +2,7 @@
 
 DEBUG = True
 
-class DebugPrint:
+class DebugPrint: # pylint: disable-next=R0903,
     """Prints the arguments if debug_state is True."""
     def __init__(self, debug_state: bool = DEBUG):
         """Initializes the DebugPrint class."""
